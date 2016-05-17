e1b283b72f23ac4b37c9c1cad9bc222b34184142
Min Lee <minlee@Zipcoder04s-MacBook-Pro.local>
2016-05-16 22:49:53 -0400

    Part3

--- src/main/java/io/minlee/PhoneBook.java
+++ src/main/java/io/minlee/PhoneBook.java
@@ -1,5 +1,6 @@
 package io.minlee;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -9,43 +10,58 @@ import java.util.Set;
  */
 public class PhoneBook {
 
-    private Map<String,String> phoneEntries;
+    private Map<String,ArrayList<String>> phoneEntries;
 
     public PhoneBook(){
-        phoneEntries = new HashMap<String, String>();
+        phoneEntries = new HashMap<String,ArrayList<String>>();
     }
 
-    public void addEntry(String name, String phoneNumber) {
-        phoneEntries.put(name,phoneNumber);
-
+    public void addEntry(String name, ArrayList<String> phoneNumbers) {
+        phoneEntries.put(name,phoneNumbers);
     }
 
-    public String lookup(String name) {
+    public ArrayList<String> lookup(String name) {
         return phoneEntries.get(name);
     }
 
-    public void removeEntry(String name) {
+    public void removeRecord(String name) {
         phoneEntries.remove(name);
+    }
+    public void removePhoneNumber(String number) {
+        for(Map.Entry<String,ArrayList<String>> entry : phoneEntries.entrySet()){
+            for(String phoneNumber : entry.getValue()){
+                if(phoneNumber == number){
+                    entry.getValue().remove(phoneNumber);
+                }
+            }
+        }
     }
     public String listNumbers(){
         String numbers = "";
-        for(String value : phoneEntries.values()){
-            numbers +=value+"\n";
+        for(Map.Entry<String,ArrayList<String>> entry : phoneEntries.entrySet()){
+            for(String phoneNumber : entry.getValue()){
+                numbers += phoneNumber+"\n";
+            }
         }
         return numbers;
     }
     public String listEntries(){
         String entries = "";
-        for(Map.Entry<String,String> entry : phoneEntries.entrySet()){
-            entries += entry.getKey()+"\n" + entry.getValue()+"\n";
+        for(Map.Entry<String,ArrayList<String>> entry : phoneEntries.entrySet()){
+            entries += entry.getKey()+"\n";
+            for(String phoneNumber : entry.getValue()){
+                entries += phoneNumber+"\n";
+            }
         }
         return entries;
     }
 
     public String reverseLookup(String number) {
-        for(Map.Entry<String,String> entry : phoneEntries.entrySet()){
-            if(entry.getValue() == number){
-                return entry.getKey();
+        for(Map.Entry<String,ArrayList<String>> entry : phoneEntries.entrySet()){
+            for(String phoneNumber : entry.getValue()){
+                if(phoneNumber == number){
+                    return entry.getKey();
+                }
             }
         }
         return null;
--- src/test/java/io/minlee/PhoneBookSpec.java
+++ src/test/java/io/minlee/PhoneBookSpec.java
@@ -4,17 +4,22 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * Created by minlee on 5/16/16.
  */
 public class PhoneBookSpec {
 
     PhoneBook phoneBook;
-
+    ArrayList<String> phoneNumbers;
     @Before
     public void initialize() {
         phoneBook = new PhoneBook();
-        phoneBook.addEntry("Min", "856-357-7458");
+        phoneNumbers = new ArrayList<>();
+        phoneNumbers.add("856-357-7458");
+        phoneNumbers.add("856-357-7459");
+        phoneBook.addEntry("Min", phoneNumbers);
     }
     @Test
     public void reverseLookupTest(){
@@ -24,40 +29,59 @@ public class PhoneBookSpec {
     }
     @Test
     public void lookupTest() {
-        String expectedNumber = "856-357-7458";
-        String actualNumber = phoneBook.lookup("Min");
-        assertEquals(expectedNumber, actualNumber);
+        ArrayList<String> expectedNumbers = new ArrayList<>();
+        expectedNumbers.add("856-357-7458");
+        expectedNumbers.add("856-357-7459");
+        ArrayList<String> actualNumbers = phoneBook.lookup("Min");
+        assertEquals(expectedNumbers, actualNumbers);
     }
 
     @Test
     public void addEntryTest() {
-        phoneBook.addEntry("Joe", "856-357-7412");
-        String expectedNumber = "856-357-7412";
-        String actualNumber = phoneBook.lookup("Joe");
-        assertEquals(expectedNumber, actualNumber);
+        ArrayList<String> joeNumbers = new ArrayList<>();
+        joeNumbers.add("856-357-1111");
+        joeNumbers.add("856-357-2222");
+        phoneBook.addEntry("Joe", joeNumbers);
+        ArrayList<String> expectedNumbers = new ArrayList<>();
+        expectedNumbers.add("856-357-1111");
+        expectedNumbers.add("856-357-2222");
+        ArrayList<String> actualNumbers = phoneBook.lookup("Joe");
+        assertEquals(expectedNumbers, actualNumbers);
     }
 
     @Test
-    public void removeEntryTest() {
-        phoneBook.removeEntry("Min");
-        String actualNumber = phoneBook.lookup("Min");
+    public void removeRecordTest() {
+        phoneBook.removeRecord("Min");
+        ArrayList<String> actualNumber = phoneBook.lookup("Min");
         assertNull(actualNumber);
     }
 
     @Test
     public void listNumbersTest() {
-        phoneBook.addEntry("Joe", "856-357-7412");
-        String expectedNumbers = "856-357-7412\n856-357-7458\n";
+        ArrayList<String> joeNumbers = new ArrayList<>();
+        joeNumbers.add("856-357-1111");
+        joeNumbers.add("856-357-2222");
+        phoneBook.addEntry("Joe", joeNumbers);
+        String expectedNumbers = "856-357-1111\n856-357-2222\n856-357-7458\n856-357-7459\n";
         String actualNumbers = phoneBook.listNumbers();
         assertEquals(expectedNumbers, actualNumbers);
     }
 
     @Test
     public void listEntriesTest() {
-        phoneBook.addEntry("Joe", "856-357-7412");
-        String expectedNumbers = "Joe\n856-357-7412\nMin\n856-357-7458\n";
-        String actualNumbers = phoneBook.listEntries();
-        assertEquals(expectedNumbers, actualNumbers);
+        ArrayList<String> joeNumbers = new ArrayList<>();
+        joeNumbers.add("856-357-1111");
+        joeNumbers.add("856-357-2222");
+        phoneBook.addEntry("Joe", joeNumbers);
+        String expectedEntries = "Joe\n856-357-1111\n856-357-2222\nMin\n856-357-7458\n856-357-7459\n";
+        String actualEntries = phoneBook.listEntries();
+        assertEquals(expectedEntries, actualEntries);
     }
-
+    @Test
+    public void removePhoneNumberTest() {
+        phoneBook.removePhoneNumber("856-357-7458");
+        String expectedNumbers = "856-357-7459\n";
+        String actualNumbers = phoneBook.listNumbers();
+        assertEquals(expectedNumbers,actualNumbers);
+    }
 }
